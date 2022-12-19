20b3cd0c8e43e70dd137f4365e687b955ee25aa1
Jake Pittis <jakepittis@gmail.com>
2022-12-19 12:54:36 -0500

    Remove redundant static lifetimes for constants

--- rust-control-plane/src/snapshot/type_url.rs
+++ rust-control-plane/src/snapshot/type_url.rs
@@ -4,17 +4,17 @@ macro_rules! prefix {
     };
 }
 
-pub const ENDPOINT: &'static str = prefix!("envoy.config.endpoint.v3.ClusterLoadAssignment");
-pub const CLUSTER: &'static str = prefix!("envoy.config.cluster.v3.Cluster");
-pub const ROUTE: &'static str = prefix!("envoy.config.route.v3.RouteConfiguration");
-pub const VIRTUAL_HOST: &'static str = prefix!("envoy.config.route.v3.VirtualHost");
-pub const LISTENER: &'static str = prefix!("envoy.config.listener.v3.Listener");
-pub const SECRET: &'static str = prefix!("envoy.extensions.transport_sockets.tls.v3.Secret");
-pub const RUNTIME: &'static str = prefix!("envoy.service.runtime.v3.Runtime");
-pub const SCOPED_ROUTE: &'static str = prefix!("envoy.config.route.v3.ScopedRouteConfiguration");
-pub const EXTENSION_CONFIG: &'static str = prefix!("envoy.config.core.v3.TypedExtensionConfig");
+pub const ENDPOINT: &str = prefix!("envoy.config.endpoint.v3.ClusterLoadAssignment");
+pub const CLUSTER: &str = prefix!("envoy.config.cluster.v3.Cluster");
+pub const ROUTE: &str = prefix!("envoy.config.route.v3.RouteConfiguration");
+pub const VIRTUAL_HOST: &str = prefix!("envoy.config.route.v3.VirtualHost");
+pub const LISTENER: &str = prefix!("envoy.config.listener.v3.Listener");
+pub const SECRET: &str = prefix!("envoy.extensions.transport_sockets.tls.v3.Secret");
+pub const RUNTIME: &str = prefix!("envoy.service.runtime.v3.Runtime");
+pub const SCOPED_ROUTE: &str = prefix!("envoy.config.route.v3.ScopedRouteConfiguration");
+pub const EXTENSION_CONFIG: &str = prefix!("envoy.config.core.v3.TypedExtensionConfig");
 
-pub const ANY_TYPE: &'static str = "";
+pub const ANY_TYPE: &str = "";
 
 #[cfg(test)]
 mod tests {
