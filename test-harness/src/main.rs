8aaf18228f7cc7ba16ca9acc63bdb9c8588823ec
Jake Pittis <jakepittis@gmail.com>
2023-01-02 13:06:34 -0500

    wip

--- test-harness/src/main.rs
+++ test-harness/src/main.rs
@@ -1,4 +1,5 @@
 mod model;
+mod process;
 
 use clap::Parser;
 use tracing::info;
---
+++ test-harness/src/process.rs
@@ -0,0 +1,66 @@
+use std::io;
+use std::process::{Child, Command, Stdio};
+
+struct Config {
+    ads: bool,
+    delta: bool,
+    service_node: String,
+    service_cluster: String,
+}
+
+struct Process {
+    config: Config,
+    child: Option<Child>,
+}
+
+impl Process {
+    pub fn new(config: Config) -> Self {
+        Self {
+            config,
+            child: None,
+        }
+    }
+
+    pub fn spawn(&mut self) -> io::Result<()> {
+        let child = self.command().spawn()?;
+        self.child = Some(child);
+        Ok(())
+    }
+
+    fn command(&self) -> Command {
+        let mut cmd = Command::new("envoy");
+        cmd.args([
+            "-c",
+            &config_path(self.config.ads, self.config.delta),
+            "--service-node",
+            &self.config.service_node,
+            "--service-cluster",
+            &self.config.service_cluster,
+        ])
+        .stdout(Stdio::null())
+        .stderr(Stdio::null());
+        cmd
+    }
+
+    fn kill(&mut self) -> io::Result<()> {
+        self.child.as_mut().expect("No child found").kill()
+    }
+}
+
+impl Drop for Process {
+    fn drop(&mut self) {
+        self.kill().expect("Failed to kill process")
+    }
+}
+
+fn config_path(ads: bool, delta: bool) -> String {
+    if !delta && ads {
+        return "envoy/ads.yaml".to_string();
+    } else if delta && !ads {
+        return "envoy/delta_config.yaml".to_string();
+    } else if delta && ads {
+        return "envoy/delta_ads.yaml".to_string();
+    } else {
+        return "envoy/config.yaml".to_string();
+    }
+}
